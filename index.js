77900949ddfd66c47305c55485f1818ed784b94c
asha mohamed <falastine924@gmail.com>
2025-06-03 23:27:52 +0300

    index.html

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+// Multiply
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2; // 2 * 31 = 62
+
+// Random
+const random = Math.floor(Math.random() * 10) + 1;
+
+// Mod
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4; // 10 % 6 = 4
+
+// Max
+// Finds the highest number in the given set
+const max = Math.max(4, 10, 20); // max equals 20
